a6428a798cdedb0d971f686d89810c65b55f7ac4
Joaquin Niembro Bueno Gómez <Joaquin.Bueno@definityfirst.com>
2021-05-12 14:32:50 -0500

    cool

---
+++ lib/foodApp_web/controllers/fallback_controller.ex
@@ -0,0 +1,16 @@
+defmodule FoodAppWeb.FallbackController do
+  use FoodAppWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(FoodAppWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> json(%{error: "Login error"})
+  end
+end
